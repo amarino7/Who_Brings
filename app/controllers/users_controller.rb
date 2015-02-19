c97aa08d306687ac1889be684027ba2866cf3275
Allison Marino <amarino_us@yahoo.com>
2015-02-19 10:18:58 -0800

    event show page add styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	# before_action :logged_in?, only: [:show]
+	before_action :logged_in?, only: [:show]
 
   def index
     @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -28,3 +28,37 @@ Rails.application.routes.draw do
 
   
 end
+
+#  Prefix Verb     URI Pattern                                     Controller#Action
+#         root GET      /                                               site#index
+#              GET|POST /auth/:provider/callback(.:format)              omniauth#create
+# auth_failure GET|POST /auth/failure(.:format)                         redirect(301, /)
+#        login GET      /login(.:format)                                sessions#new
+#     sessions POST     /sessions(.:format)                             sessions#create
+#      sign_up GET      /sign_up(.:format)                              users#new
+#       logout GET      /logout(.:format)                               omniauth#destroy
+#        users GET      /users(.:format)                                users#index
+#              POST     /users(.:format)                                users#create
+#     new_user GET      /users/new(.:format)                            users#new
+#    edit_user GET      /users/:id/edit(.:format)                       users#edit
+#         user GET      /users/:id(.:format)                            users#show
+#              PATCH    /users/:id(.:format)                            users#update
+#              PUT      /users/:id(.:format)                            users#update
+#              DELETE   /users/:id(.:format)                            users#destroy
+#      contact GET      /contact(.:format)                              site#contact
+#        about GET      /about(.:format)                                site#about
+#       events GET      /events(.:format)                               events#index
+#              POST     /events(.:format)                               events#create
+#    new_event GET      /events/new(.:format)                           events#new
+#   edit_event GET      /events/:id/edit(.:format)                      events#edit
+#        event GET      /events/:id(.:format)                           events#show
+#              PATCH    /events/:id(.:format)                           events#update
+#              PUT      /events/:id(.:format)                           events#update
+#              DELETE   /events/:id(.:format)                           events#destroy
+#        tasks GET      /events/:event_id/tasks(.:format)               tasks#index
+#     new_task GET      /events/:event_id/tasks/new(.:format)           tasks#new
+#         task GET      /events/:event_id/tasks/:task_id(.:format)      tasks#show
+#    edit_task GET      /events/:event_id/tasks/:task_id/edit(.:format) tasks#edit
+#              POST     /events/:event_id/tasks(.:format)               tasks#create
+#  task_update POST     /events/:event_id/tasks/:task_id(.:format)      tasks#update
+# destroy_task DELETE   /events/:event_id/tasks/:task_id(.:format)      tasks#destroy
